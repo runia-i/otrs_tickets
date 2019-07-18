2242b3c9f9430d7330e79fcda5efcf4c4633fe75
Roman Khafizov <hafaml@mail.ru>
2019-07-18 16:49:55 +0500

    update token decode

--- tele_bot.py
+++ tele_bot.py
@@ -2,7 +2,7 @@ import telebot
 import base64
 
 
-bot = telebot.TeleBot(base64.decode('NjQwODIzNzMzOkFBRlFhS0lGajJPMjZyT0VTaFQtbFJERUJlZzVodUY4ZUVj').decode('utf-8'))
+bot = telebot.TeleBot(base64.b64decode('NjQwODIzNzMzOkFBRlFhS0lGajJPMjZyT0VTaFQtbFJERUJlZzVodUY4ZUVj').decode('utf-8'))
 
 
 @bot.message_handler(commands=['start'])
