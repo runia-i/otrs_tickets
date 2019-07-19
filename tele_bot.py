67085ebaa898b329fad2e5e332a6a56e919785dc
Roman Khafizov <hafaml@mail.ru>
2019-07-19 11:38:14 +0500

    update bot message reply

--- tele_bot.py
+++ tele_bot.py
@@ -1,13 +1,13 @@
 import telebot
 import base64
 
-
-bot = telebot.TeleBot(base64.b64decode('NjQwODIzNzMzOkFBRlFhS0lGajJPMjZyT0VTaFQtbFJERUJlZzVodUY4ZUVj').decode('utf-8'))
+token = base64.b64decode('NjQwODIzNzMzOkFBRlFhS0lGajJPMjZyT0VTaFQtbFJERUJlZzVodUY4ZUVj').decode('utf-8')
+bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
-    bot.send_message(message.chat_id, 'Ты написал мне /start, Бот запущен')
+    bot.reply_to(message, 'Ты написал мне /start, Бот запущен')
 
 
 bot.polling()
