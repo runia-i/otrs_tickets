fd647b715856136251f266926634eb8013be9653
Roman Khafizov <hafaml@mail.ru>
2019-09-30 16:00:31 +0500

    Изменена файловая структура проекта

--- otrs_parse.py
+++ otrs_parse.py
@@ -1,23 +1,20 @@
 import time
-import base64
 import telebot
 import pandas as pd
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from config import pw, login, token
 
 
-pw = base64.b64decode('YTYzYmwyNDEyOTI=').decode('utf-8')
-login = base64.b85decode('YiMC+X?kyVE^=u=VqtS=Yj1LNZ*z2EZZ2|l').decode("utf-8")
 url = "https://gku-service.bashkortostan.ru/otrs/index.pl?Action=AgentTicketQueue;QueueID=1;View=Small;Filter=All;;SortBy=Age;OrderBy=Down"
 current_tickets = []
-token = base64.b64decode('NjQwODIzNzMzOkFBRlFhS0lGajJPMjZyT0VTaFQtbFJERUJlZzVodUY4ZUVj').decode('utf-8')
 bot = telebot.TeleBot(token)
 
 
 def init_driver():
     """
-    Initiation Chrome Webdriver
+    Initiation Chrome Webdriver without GUI
     """
     options = Options()
     options.headless = True
@@ -43,12 +40,16 @@ def tickets_handler(tickets_list: list):
         message_sender(new_tickets)
         print(new_tickets, sep=" ")
 
+def do_time_now():
+    time_now = int(datetime.utcnow().strftime("%H"))
+    if 2 < time_now < 15:
+        return
 
-def lookup(driver, url):
-    while True
+
+def main(driver, url):
+    while True:
         #  Вычисляем текущее время и проверяем является ли оно рабочимм(по UTC).
-        time_now = int(datetime.utcnow().strftime("%H"))
-        if 2 < time_now < 15:
+
             driver.get(url)
             source = driver.page_source
             # Проверяем, что перед нами страница авторизации и вводим логин/пароль
@@ -59,7 +60,7 @@ def lookup(driver, url):
                 elem_pw.send_keys(pw)
                 elem_pw.submit()
             else:
-                # Считываем с html страницы таблицу с заявками, записываем в переменную tickets
+                # Считываем с html страницы таблицу с заявками, преобразуем из DateFrame в list
                 tables = pd.read_html(source, header=0)[0]
                 tickets = tables['Ticket#'].values.tolist()
                 tickets_handler(tickets)
@@ -70,7 +71,7 @@ def lookup(driver, url):
 if __name__ == "__main__":
     try:
         driver = init_driver()
-        lookup(driver, url)
+        main(driver, url)
     except KeyboardInterrupt:
         driver.quit()
         exit()
